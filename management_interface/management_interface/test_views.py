be5eaa4b9824746902d6d74d6e92fcb63ab046b6
Grzegorz Ludorowski <gregory.ludorowski@thepsc.co.uk>
2023-03-19 22:16:05 +0100

    feat: Endpoint for getting Care Providers data [HANS-292]

---
+++ management_interface/management_interface/test_views.py
@@ -0,0 +1,49 @@
+import json
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import RegisteredManager
+
+
+class CareProviderLocationTests(TestCase):
+    def setUp(self) -> None:
+        self.manager = RegisteredManager.objects.create(
+            given_name="Jehosephat", family_name="McGibbons", cqc_registered_manager_id="My CQC RegsiteredManagerID"
+        )
+        self.location = self.manager.careproviderlocation_set.create(
+            name="My Location Name",
+            email="nosuchaddress@nhs.net",
+            ods_code="My Ods Code",
+            cqc_location_id="My CQC Location ID",
+        )
+        self.care_recipient = self.location.carerecipient_set.create(
+            subscription_id="42", provider_reference_id="foobar"
+        )
+        self.care_recipient.nhs_number = "password"
+        self.care_recipient.save()
+
+    def test_car_care_provider_location_search_not_allowed(self):
+        url_ = reverse("care_provider_search")
+        response = self.client.get(url_, {"_careRecipientPseudoId": self.care_recipient.nhs_number_hash})
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+        self.assertEqual(json.loads(response.content)["issue"][0]["code"], "not-allowed")
+
+    def test_successful_car_care_provider_location_search(self):
+        url_ = reverse("care_provider_search")
+        response = self.client.post(url_, {"_careRecipientPseudoId": self.care_recipient.nhs_number_hash})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(json.loads(response.content)["name"], self.location.name)
+
+    def test_car_care_provider_location_search_not_found(self):
+        url_ = reverse("care_provider_search")
+        response = self.client.post(url_, {"_careRecipientPseudoId": "not_existing_id"})
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertEqual(json.loads(response.content)["issue"][0]["code"], "not-found")
+
+    def test_car_care_provider_location_search_bad_request(self):
+        url_ = reverse("care_provider_search")
+        response = self.client.post(url_, {"_invalid_query_parameter": "not_existing_id"})
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(json.loads(response.content)["issue"][0]["code"], "required")
---
+++ management_interface/management_interface/views.py
@@ -0,0 +1,48 @@
+import json
+from http import HTTPStatus
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from fhir.resources.contactpoint import ContactPoint
+from fhir.resources.operationoutcome import OperationOutcome, OperationOutcomeIssue
+from fhir.resources.organization import Organization
+
+from .models import CareProviderLocation
+
+
+@csrf_exempt
+def care_provider_search(request):
+    if request.method == "POST":
+
+        try:
+            nhs_number_hash = request.POST["_careRecipientPseudoId"]
+        except KeyError:
+            operation_outcome_issue = OperationOutcomeIssue(
+                severity="error",
+                code="required",
+                diagnostics="Required search parameter was missing: _careRecipientPseudoId",
+            )
+            operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
+            return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.BAD_REQUEST)
+
+        try:
+            care_provider = CareProviderLocation.objects.get(carerecipient__nhs_number_hash=nhs_number_hash)
+        except CareProviderLocation.DoesNotExist:
+            operation_outcome_issue = OperationOutcomeIssue(
+                severity="error",
+                code="not-found",
+                diagnostics="No subscription was found on the system for the given psuedonymous identifier",
+            )
+            operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
+            return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.NOT_FOUND)
+
+        fhir_contact_point = ContactPoint(system="email", value=care_provider.email, use="work")
+        fhir_organization = Organization(name=care_provider.name, telecom=[fhir_contact_point])
+        return JsonResponse(json.loads(fhir_organization.json()), safe=False)
+
+    # if not allowed method was used on this endpoint
+    operation_outcome_issue = OperationOutcomeIssue(
+        severity="error", code="not-allowed", diagnostics="Method not allowed"
+    )
+    operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
+    return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.METHOD_NOT_ALLOWED)
