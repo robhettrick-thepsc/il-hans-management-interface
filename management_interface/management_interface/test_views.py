9af11852c023efc6027f33aaae7e042e1cb78ae1
Grzegorz Ludorowski <gregory.ludorowski@thepsc.co.uk>
2023-03-22 09:57:56 +0100

    Satisfy PR comments

--- management_interface/management_interface/test_views.py
+++ management_interface/management_interface/test_views.py
@@ -1,4 +1,3 @@
-import json
 from http import HTTPStatus
 
 from django.test import TestCase
@@ -23,9 +22,11 @@ class CareProviderLocationTests(TestCase):
         )
         self.care_recipient.nhs_number = "password"
         self.care_recipient.save()
+
     def assertFailure(self, response, expected_status_code, expected_code):
         self.assertEqual(response.status_code, expected_status_code)
         self.assertEqual(response.json()["issue"][0]["code"], expected_code)
+
     def test_search_get_method_not_allowed(self):
         url = reverse("care_provider_search")
         response = self.client.get(url, {"_careRecipientPseudoId": self.care_recipient.nhs_number_hash})
--- management_interface/management_interface/views.py
+++ management_interface/management_interface/views.py
@@ -1,4 +1,3 @@
-import json
 from http import HTTPStatus
 
 from django.http import JsonResponse
@@ -10,6 +9,16 @@ from fhir.resources.organization import Organization
 from .models import CareProviderLocation
 
 
+def failure_response(status, code, diagnostics):
+    operation_outcome_issue = OperationOutcomeIssue(
+        severity="error",
+        code=code,
+        diagnostics=diagnostics,
+    )
+    operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
+    return JsonResponse(operation_outcome.dict(), status=status)
+
+
 @csrf_exempt
 def care_provider_search(request):
     if request.method == "POST":
@@ -17,32 +26,29 @@ def care_provider_search(request):
         try:
             nhs_number_hash = request.POST["_careRecipientPseudoId"]
         except KeyError:
-            operation_outcome_issue = OperationOutcomeIssue(
-                severity="error",
+            return failure_response(
+                status=HTTPStatus.BAD_REQUEST,
                 code="required",
                 diagnostics="Required search parameter was missing: _careRecipientPseudoId",
             )
-            operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
-            return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.BAD_REQUEST)
 
         try:
             care_provider = CareProviderLocation.objects.get(carerecipient__nhs_number_hash=nhs_number_hash)
         except CareProviderLocation.DoesNotExist:
-            operation_outcome_issue = OperationOutcomeIssue(
-                severity="error",
+            return failure_response(
+                status=HTTPStatus.NOT_FOUND,
                 code="not-found",
-                diagnostics="No subscription was found on the system for the given psuedonymous identifier",
+                diagnostics="No subscription was found on the system for the given pseudonymous identifier",
             )
-            operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
-            return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.NOT_FOUND)
 
         fhir_contact_point = ContactPoint(system="email", value=care_provider.email, use="work")
         fhir_organization = Organization(name=care_provider.name, telecom=[fhir_contact_point])
-        return JsonResponse(json.loads(fhir_organization.json()), safe=False)
+        return JsonResponse(fhir_organization.dict())
 
     # if not allowed method was used on this endpoint
-    operation_outcome_issue = OperationOutcomeIssue(
-        severity="error", code="not-allowed", diagnostics="Method not allowed - _search only supports POST"
-    )
-    operation_outcome = OperationOutcome(issue=[operation_outcome_issue])
-    return JsonResponse(json.loads(operation_outcome.json()), status=HTTPStatus.METHOD_NOT_ALLOWED)
+    else:
+        return failure_response(
+            status=HTTPStatus.METHOD_NOT_ALLOWED,
+            code="not-allowed",
+            diagnostics="Method not allowed - _search only supports POST",
+        )
